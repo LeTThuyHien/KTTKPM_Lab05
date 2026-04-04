396695ef3b91ddb2721f9f361b24fb5f0854b21c
LETHITHUYHIEN <lethithuyhien@MacBook-Air-cua-LETHITHUYHIEN.local>
2026-04-04 15:03:30 +0700

    update phan 1

--- compose-demo/app.py
+++ compose-demo/app.py
@@ -13,3 +13,7 @@ cache = redis.Redis(
 def hello():
     count = cache.incr("hits")
     return f"Hello from Docker! I have been seen {count} time(s).\n"
+
+# 🔥 BẮT BUỘC có đoạn này
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
